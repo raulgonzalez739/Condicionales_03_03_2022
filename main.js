be5f985953400dea6c774cf4ec22dcb646a8e786
raulgonzalez739 <fibonacci739@gmail.com>
2022-03-05 10:15:25 -0500

    Algoritmo altura

---
+++ main.js
@@ -0,0 +1,11 @@
+let altura;
+altura = prompt('Por favor ingrese su altura (en centimetros):','');
+if (altura <= 150) 
+    {alert('Persona de altura baja');}
+    else {if (altura<=170) 
+            {alert('Persona de altura media');}       
+            else { if (altura > 170) {alert('Persona alta');
+                                    }
+
+                 }
+         }      
