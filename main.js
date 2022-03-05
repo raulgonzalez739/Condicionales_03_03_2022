5a6ef52ba31271059b68db40d356b8b59f51fc54
raulgonzalez739 <fibonacci739@gmail.com>
2022-03-05 12:17:14 -0500

    Bombillo v1

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
-let día;
-día = prompt('Ingrese el numero del día de la semana [1-7]', '');
-if (día == 1) { alert('Lunes');}
-else if (día==2){alert('Martes');}
-else if (día==3){alert('Miercoles');}
-else if (día==4){alert('Jueves');}
-else if (día==5){alert('Viernes');}
-else if (día==6){alert('Sábado');}
-else if (día==7){alert('Domingo');}
-else {alert('Valor invalido');}
+let enciende;
+enciende = prompt('El bombillo enciende?? (Si/No):', '');
+if(enciende == 'Si'){alert('Felcidades, su bombillo está ok')
+                   enciende = 'Si';}
+else { alert('Revise la conexión')
+        let conectado;
+        conectado = prompt('El bombillo está conectado?? (Si/No):', '');
+       if (conectado== 'Si'){alert('Revisar si el Bombillo está quemado');
+                            quemado=prompt('El bombillo está quemado?? (Si/No):', '');
+                            if (quemado=='No') {alert('Llevelo a servicio técnico');}
+                                else {alert('Cambiar el bombilo');}
+                                enciende = prompt('El bombillo enciende?? (Si/No):', '');
+}
+       }
