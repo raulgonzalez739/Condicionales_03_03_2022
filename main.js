e7b01e697c5729f1d2321de19504fadfe71013a3
raulgonzalez739 <fibonacci739@gmail.com>
2022-03-05 11:09:42 -0500

    Algoritmo días de la semana

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
-let sueldo;
-sueldo = prompt('Por favor ingrese su sueldo (Sin puntos ni comas):','');
-
-if (sueldo > 3000) { alert('Esta persona debe abonar impuestos');
-                    }
-                    else {alert('Está al día con hacienda')
-                          }
+let día;
+día = prompt('Ingrese el numero del día de la semana [1-7]', '');
+if (día == 1) { alert('Lunes');}
+else if (día==2){alert('Martes');}
+else if (día==3){alert('Miercoles');}
+else if (día==4){alert('Jueves');}
+else if (día==5){alert('Viernes');}
+else if (día==6){alert('Sábado');}
+else if (día==7){alert('Domingo');}
+else {alert('Valor invalido');}
