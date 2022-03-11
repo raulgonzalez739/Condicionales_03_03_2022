fe0548a0a001c3e6b1c5c0ad1b0056d0d9f3c003
raulgonzalez739 <fibonacci739@gmail.com>
2022-03-10 19:03:34 -0500

    Bombillo ok

--- main.js
+++ main.js
@@ -1,14 +1,46 @@
-let enciende;
+let enciende, conectado;
+
+// if (enciende == 'Si') {
+//     alert('Felcidades, su bombillo está ok');
+// }
+// else {
 enciende = prompt('El bombillo enciende?? (Si/No):', '');
-if(enciende == 'Si'){alert('Felcidades, su bombillo está ok')
-                   enciende = 'Si';}
-else { alert('Revise la conexión')
-        let conectado;
-        conectado = prompt('El bombillo está conectado?? (Si/No):', '');
-       if (conectado== 'Si'){alert('Revisar si el Bombillo está quemado');
-                            quemado=prompt('El bombillo está quemado?? (Si/No):', '');
-                            if (quemado=='No') {alert('Llevelo a servicio técnico');}
-                                else {alert('Cambiar el bombilo');}
-                                enciende = prompt('El bombillo enciende?? (Si/No):', '');
+while (enciende=='No') {
+    alert('Comprobar conexión')
+    conectado = prompt('Esta conectado?? (Si/No):','');
+    if (conectado=='No') {
+        enciende = prompt('El bombillo enciende?? (Si/No):', '');            
+    }
+    else{
+        alert('Comprobar Bombillo');
+        quemado= prompt('Esta quemado?? (Si/No):','');
+        if(quemado == "No"){
+            alert('LLevar a servicio');
+            enciende = '.';
+        }else{
+            alert('Cambiar Bombillo');
+            enciende = prompt('El bombillo enciende?? (Si/No):', '');
+        } 
+    }
 }
-       }
+if(enciende != "."){
+    alert('Felcidades, su bombillo está ok');
+}
+
+    // do {
+    //     alert('Comprobar conexión');
+    //     conectado = prompt('Esta conectado?? (Si/No):','');
+    //     if (conectado == 'Si') {
+    //         alert('Comprobar Bombillo');
+    //         quemado = prompt('El bombillo está quemado??:','');
+    //     }
+    //     else{
+    //         alert('Conectar');
+            
+    //         do {
+    //             enciende = prompt('El bombillo enciende?? (Si/No):', '');
+    //         } while (conectado=='No');
+            
+    //     }
+    // } while (enciende=='No');
+// }
